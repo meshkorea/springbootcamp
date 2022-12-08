b5777f95b256fb4261607e848a46241a738e6b56
twkim5235 <sssunggure@naver.com>
2022-12-08 10:39:33 +0900

    [FIX] change template to product

---
+++ product/src/main/java/com/vroong/product/application/port/out/ExampleRepository.java
@@ -0,0 +1,9 @@
+package com.vroong.product.application.port.out;
+
+import com.vroong.product.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ExampleRepository extends JpaRepository<Example, Long>,
+    JpaSpecificationExecutor<Example> {
+}
