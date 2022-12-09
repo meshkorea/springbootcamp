87794d1178c8f2e0b687986a3885650a2b60c662
youngiggy <youngiggy@gmail.com>
2022-12-09 09:12:11 +0900

    테이블 scheme 추가

--- order/src/main/resources/db/schema.sql
+++ order/src/main/resources/db/schema.sql
@@ -21,13 +21,29 @@ CREATE TABLE IF NOT EXISTS persistent_events
     KEY           IDX_STATUS_EVENTTYPE (status,event_type)*/
 );
 
-CREATE TABLE IF NOT EXISTS examples
+CREATE TABLE IF NOT EXISTS orders
 (
-    id         bigint(20) NOT NULL AUTO_INCREMENT,
-    title      varchar(255) DEFAULT NULL,
-    created_at timestamp(3) NOT NULL,
-    updated_at timestamp(3) NOT NULL,
-    created_by varchar(40)  NOT NULL,
-    updated_by varchar(40)  NOT NULL,
+    id               bigint NOT NULL AUTO_INCREMENT,
+    status           tinyint unsigned NOT NULL DEFAULT 0 COMMENT '오더 상태',
+    sender_name      varchar(32) NOT NULL COMMENT '발신자 이름',
+    sender_phone     varchar(20) NOT NULL COMMENT '발신자 전화번호',
+    sender_address   varchar(255) NOT NULL COMMENT '발신자 주소',
+    receiver_name    varchar(32) NOT NULL COMMENT '수신자 이름',
+    receiver_phone   varchar(20) NOT NULL COMMENT '수신자 전화번호',
+    receiver_address varchar(255) NOT NULL COMMENT '수신자 주소',
+    created_at       timestamp NOT NULL,
+    updated_at       timestamp NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS order_items
+(
+    id               bigint NOT NULL AUTO_INCREMENT,
+    order_id         bigint NOT NULL COMMENT '주문 번호',
+    product_id       bigint NOT NULL COMMENT '상품 번호',
+    product_name     varchar(60) NOT NULL COMMENT '상품명',
+    quantity         smallint NOT NULL COMMENT '수량',
+    created_at       timestamp NOT NULL,
+    updated_at       timestamp NOT NULL,
     PRIMARY KEY (id)
 );
