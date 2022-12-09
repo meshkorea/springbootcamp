1bbf773cb2b8178199f85cdf97ad0d9a9a7fddd3
appkr <juwonkim@me.com>
2022-12-09 11:37:57 +0900

    removed template

--- template/src/main/java/com/vroong/template/config/MessagingConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.vroong.template.config;
-
-import com.vroong.template.application.port.out.ProducerChannel;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-
-@EnableBinding(value = {ProducerChannel.class})
-public class MessagingConfiguration {
-}
