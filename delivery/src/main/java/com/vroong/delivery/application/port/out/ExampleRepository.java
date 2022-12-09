a3ed7788ef3e45e251200dc085e4913afbb482df
appkr <juwonkim@me.com>
2022-12-09 14:07:11 +0900

    Delivery : 빌드 성공

--- delivery/src/main/java/com/vroong/delivery/application/port/out/ExampleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vroong.delivery.application.port.out;
-
-import com.vroong.delivery.domain.Example;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface ExampleRepository extends JpaRepository<Example, Long>,
-    JpaSpecificationExecutor<Example> {
-}
