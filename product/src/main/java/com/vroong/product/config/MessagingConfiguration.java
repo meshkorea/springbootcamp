1d36e3354aa621afddd6103a2f24c1bfcff57184
appkr <juwonkim@me.com>
2022-12-09 11:40:13 +0900

    product

---
+++ product/src/main/java/com/vroong/product/config/MessagingConfiguration.java
@@ -0,0 +1,8 @@
+package com.vroong.product.config;
+
+import com.vroong.product.application.port.out.ProducerChannel;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+@EnableBinding(value = {ProducerChannel.class})
+public class MessagingConfiguration {
+}
