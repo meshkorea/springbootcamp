1d3200d3c1bef8eb1265968e769e95cf7e0ddcbe
hyunseok.lee <smartbosslee@gmail.com>
2022-12-16 11:39:31 +0900

    재고가 부족하지 않을 때만 재고를 차감.

--- product/src/main/java/com/vroong/product/application/MessageConsumer.java
+++ product/src/main/java/com/vroong/product/application/MessageConsumer.java
@@ -5,6 +5,7 @@ import com.vroong.order.rest.OrderLineItem;
 import com.vroong.product.application.port.in.ConsumerChannel;
 import com.vroong.product.application.port.in.OrderEvent;
 import com.vroong.product.application.port.out.OrderTemplate;
+import com.vroong.product.domain.Product;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.StreamListener;
@@ -20,15 +21,25 @@ public class MessageConsumer {
 
     @StreamListener(value = ConsumerChannel.CHANNEL, condition = "headers['messageType']=='ORDER_CREATED'")
     public void onReceiveOrderCreated(OrderEvent message) {
-        // order 를 조회해서 주문 수량 만큼 재고를 차감한다.
         Order createdOrder = orderTemplate.getOrderByOrderId(message.getOrderId());
 
         assert createdOrder.getOrderLine() != null;
         assert createdOrder.getOrderLine().getData() != null;
         for (OrderLineItem orderLine : createdOrder.getOrderLine().getData()) {
             assert orderLine.getProduct() != null;
-            productService.decreaseInventory(orderLine.getProduct().getProductId(),
-                orderLine.getQuantity());
+
+            Product product  = productService.getProduct(orderLine.getProductId());
+
+            if(product.getInventory() > orderLine.getQuantity()) {
+                productService.decreaseInventory(
+                        orderLine.getProduct().getProductId(),
+                        orderLine.getQuantity()
+                );
+
+                // Todo 재고가 변경되었다는 메시지를 발송한다.
+            } else {
+                // Todo 재고가 부족하여 처리할 수 없다는 메시지를 발송한다.
+            }
         }
     }
 
