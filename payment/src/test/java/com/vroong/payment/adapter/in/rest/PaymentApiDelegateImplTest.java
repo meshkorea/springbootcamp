459fa3ba3c0a66a05d925e126581ae72cea7f5a9
Inseok-Hong <inseok.hong@meshkorea.net>
2022-12-21 11:58:36 +0900

    test failed 수정, asyncapi spec 수정

--- payment/src/test/java/com/vroong/payment/adapter/in/rest/PaymentApiDelegateImplTest.java
+++ payment/src/test/java/com/vroong/payment/adapter/in/rest/PaymentApiDelegateImplTest.java
@@ -1,18 +1,28 @@
 package com.vroong.payment.adapter.in.rest;
 
+import static com.vroong.payment.domain.Fixtures.aPayment;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.vroong.payment.application.port.in.PaymentService;
 import com.vroong.payment.config.Constants;
+import com.vroong.payment.domain.Payment;
 import com.vroong.payment.rest.PaymentApiController;
 import com.vroong.payment.rest.PaymentApiDelegate;
 import com.vroong.payment.support.TestUtils;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -26,6 +36,7 @@ class PaymentApiDelegateImplTest {
   private MockMvc mvc;
 
   @Autowired private PaymentApiDelegate apiDelegate;
+  @MockBean private PaymentService mockService;
 
   @BeforeEach
   void setup() {
@@ -37,6 +48,8 @@ class PaymentApiDelegateImplTest {
 
   @Test
   void createPayment() throws Exception {
+    doNothing().when(mockService).checkout(any(Payment.class));
+
     final ResultActions res =
         mvc.perform(
                 post("/api/payments")
@@ -50,6 +63,9 @@ class PaymentApiDelegateImplTest {
 
   @Test
   void getPaymentList() throws Exception {
+    final List<Payment> entityList = new ArrayList<>(List.of(aPayment()));
+    when(mockService.getPaymentList(anyLong())).thenReturn(entityList);
+
     final ResultActions res =
         mvc.perform(
                 get("/api/payments/{orderId}", 1L)
