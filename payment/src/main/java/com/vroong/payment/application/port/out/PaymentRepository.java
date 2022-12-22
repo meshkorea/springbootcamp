873dc400b759b16b853092ec2b9713215762555a
Inseok-Hong <inseok.hong@meshkorea.net>
2022-12-22 11:14:25 +0900

    repository 에러 수정, test 추가

--- payment/src/main/java/com/vroong/payment/application/port/out/PaymentRepository.java
+++ payment/src/main/java/com/vroong/payment/application/port/out/PaymentRepository.java
@@ -5,12 +5,13 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
   @Query(value = "SELECT p FROM Payment p WHERE p.orderId = :orderId ORDER BY p.updatedAt DESC")
-  List<Payment> findByOrderId(Long orderId);
+  List<Payment> findByOrderId(@Param(value = "orderId") Long orderId);
 
   @Query(value = "SELECT p FROM Payment p WHERE p.orderId = :orderId AND p.status = 20")
-  Optional<Payment> findApprovedByOrderId(Long orderId);
+  Optional<Payment> findApprovedByOrderId(@Param(value = "orderId") Long orderId);
 }
--- payment/src/test/java/com/vroong/payment/application/port/out/PaymentRepositoryTest.java
+++ payment/src/test/java/com/vroong/payment/application/port/out/PaymentRepositoryTest.java
@@ -4,6 +4,7 @@ import static com.vroong.payment.domain.Fixtures.aPayment;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.vroong.payment.domain.Payment;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 class PaymentRepositoryTest {
 
-  @Autowired
-  PaymentRepository repository;
+  @Autowired PaymentRepository repository;
 
   @Test
   void save() {
@@ -24,6 +24,24 @@ class PaymentRepositoryTest {
     repository.save(payment);
 
     assertNotNull(payment.getId());
+    log.info("{}", payment);
+  }
+
+  @Test
+  void findByOrderId() {
+    repository.save(aPayment());
+    final List<Payment> paymentList = repository.findByOrderId(1L);
+
+    assertNotNull(paymentList.get(0));
+    log.info("{}", paymentList.get(0));
+  }
+
+  @Test
+  void findApprovedByOrderId() {
+    repository.save(aPayment());
+    final Payment payment = repository.findApprovedByOrderId(1L).orElse(null);
+
+    assertNotNull(payment);
     log.info("{}", payment);
   }
 }
