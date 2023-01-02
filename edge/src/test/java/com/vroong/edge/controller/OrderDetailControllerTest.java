12e2175ca612d766f7417539cff52291cb1ba362
jo-minjun-mesh <minjun.jo@meshkorea.net>
2023-01-02 18:31:32 +0900

    [edge] aggregate api 테스트 코드 작성

--- edge/src/test/java/com/vroong/edge/controller/OrderDetailControllerTest.java
+++ edge/src/test/java/com/vroong/edge/controller/OrderDetailControllerTest.java
@@ -1,5 +1,37 @@
 package com.vroong.edge.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import com.vroong.edge.application.OrderDetailService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
+import reactor.core.publisher.Mono;
+
+@WebFluxTest(OrderDetailController.class)
 class OrderDetailControllerTest {
 
+  @Autowired
+  WebTestClient webTestClient;
+
+  @MockBean
+  OrderDetailService service;
+
+  @Test
+  @WithMockUser
+  void 주문_상세_조회() {
+    given(service.getOrderDetail(any()))
+        .willReturn(Mono.just(new OrderDetailDto(null, null)));
+
+    ResponseSpec res = webTestClient.get()
+        .uri("/bootcamp/order-detail/1")
+        .exchange();
+
+    res.expectStatus().is2xxSuccessful();
+  }
 }
