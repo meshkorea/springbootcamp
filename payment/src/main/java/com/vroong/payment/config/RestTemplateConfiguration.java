a9443efa3cf4e12c2c607b1629372901051d8c11
Inseok-Hong <inseok.hong@meshkorea.net>
2023-01-27 22:29:19 +0900

    Order 서비스 호출시 JWT token 처리 추가

--- payment/src/main/java/com/vroong/payment/config/RestTemplateConfiguration.java
+++ payment/src/main/java/com/vroong/payment/config/RestTemplateConfiguration.java
@@ -1,6 +1,10 @@
 package com.vroong.payment.config;
 
+import com.vroong.payment.support.SecurityUtils;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,7 +13,16 @@ public class RestTemplateConfiguration {
 
   @Bean(value = "orderRestTemplate")
   public RestTemplate orderRestTemplate() {
-    return new RestTemplate();
+    final ClientHttpRequestInterceptor interceptor =
+        (request, body, context) -> {
+          request
+              .getHeaders()
+              .add(HttpHeaders.AUTHORIZATION, "Bearer " + SecurityUtils.getJwtTokenValue());
+
+          return context.execute(request, body);
+        };
+
+    return new RestTemplateBuilder().additionalInterceptors(interceptor).build();
   }
 
   @Bean(value = "pgRestTemplate")
